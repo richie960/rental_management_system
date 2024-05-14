007441c3de40246ac937757d9ae5e0220b8f310b
richard mwangi <141928327+richie960@users.noreply.github.com>
2024-05-15 01:24:22 +0300

    Update accessToken.php

--- kabarita/accessToken.php
+++ kabarita/accessToken.php
@@ -1,7 +1,7 @@
 <?php
 //YOU MPESA API KEYS
-$consumerKey = "K0d48Bdrtuao2kqA6MlsEhExe6vW9xFt"; //Fill with your app Consumer Key
-$consumerSecret = "T4qOWNN6Ex89lTT9"; //Fill with your app Consumer Secret
+$consumerKey = ""; //Fill with your app Consumer Key
+$consumerSecret = ""; //Fill with your app Consumer Secret
 //ACCESS TOKEN URL
 $access_token_url = 'https://api.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials';
 $headers = ['Content-Type:application/json; charset=utf8'];
